b0b7f2a354115c1f47c48570a1c3e37aa5aef137
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-09-30 11:13:39 -0400

    added slashes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $date =  date('Y-m-d', time());
 $date = str_replace('-', '/', $date);
 echo "The value of \$date: ".$date."<br>";
-
+$tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
